d542ab624285fe0ac8aed91affd406f17ef09131
Gokul <kartha.gokul@gmail.com>
2021-01-17 21:07:32 +0000

    running the script

---
+++ run.sh
@@ -0,0 +1,2 @@
+sudo docker run  -v $(pwd):/srv/pim -w /srv/pim --rm akeneo/pim-php-dev:4.0 php -d memory_limit=2G /usr/local/bin/composer create-project --prefer-dist akeneo/pim-community-standard /srv/pim "4.0.*@stable"
+
